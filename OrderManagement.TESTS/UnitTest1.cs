6139614d023b0b81e16bd99c081e00341f7d839e
arnoldasstrukcinskas <141561525+arnoldasstrukcinskas@users.noreply.github.com>
2025-08-14 19:19:32 +0300

    Add Unit Test

--- OrderManagement.TESTS/UnitTest1.cs
+++ OrderManagement.TESTS/UnitTest1.cs
@@ -1,11 +1,41 @@
+using Moq;
+using OrderManagement.BLL.Services;
+using OrderManagement.DATA.Entities;
+using OrderManagement.DATA.Repositories.Interfaces;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+
 namespace OrderManagement.TESTS
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public async Task AddProductToDatabaseTest()
         {
+            var mockProductRepository = new Mock<IProductRepository>();
 
+            var fakeProduct = new Product
+            {
+                Id = 1,
+                Name = "example",
+                Description = "Very long description",
+                Price = 9.99,
+                DiscountId = null,
+                Discount = null
+            };
+
+            mockProductRepository
+                .Setup(r => r.AddProductAsync(fakeProduct))
+                .ReturnsAsync(fakeProduct);
+
+            var result = await mockProductRepository.Object.AddProductAsync(fakeProduct);
+            Assert.Equal("example", result.Name);
+            Assert.Equal("Very long description", result.Description);
+            Assert.Equal(9.99, result.Price);
+            Assert.Null(result.Discount);
+
+            Assert.NotNull(result);
         }
     }
 }
